d7417b6437972604f3e90e45f974b70e55f58fa5
Samuel Parke <samueljparke@gmail.com>
2025-08-08 17:47:55 +0100

    corrected helper config (uint8 for decimals, int256 for price and a few other corrections)

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,14 +5,14 @@ import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script {
     struct NetworkConfig {
         address wethAddress;
         address wethPriceFeed;
     }
 
-    uint256 private constant DECIMALS = 8;
-    uint256 private constant ETH_USD_PRICE = 2000e8;
+    uint8 private constant DECIMALS = 8;
+    int256 private constant ETH_USD_PRICE = 2000e8;
     NetworkConfig public activeNetworkConfig;
 
     constructor() {
@@ -23,11 +23,11 @@ contract HelperConfig {
         }
     }
 
-    function getSepoliaConfig() public returns (NetworkConfig memory){
-        return (NetworkConfig({
+    function getSepoliaConfig() public returns (NetworkConfig memory) {
+        return NetworkConfig({
             wethAddress: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
-            wethPriceFeed:0x694AA1769357215DE4FAC081bf1f309aDC325306
-        }))
+            wethPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
     }
 
     function getAnvilConfig() public returns (NetworkConfig memory) {
@@ -35,6 +35,6 @@ contract HelperConfig {
         ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 100e8);
         MockV3Aggregator wethPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
         vm.stopBroadcast();
-        return (NetworkConfig({wethAddress: wethMock, wethPriceFeed: wethPriceFeed}));
+        return (NetworkConfig({wethAddress: address(wethMock), wethPriceFeed: address(wethPriceFeed)}));
     }
 }
--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -6,5 +6,58 @@ import {CollateralManager} from "../src/CollateralManager.sol";
 import {LendingManager} from "../src/LendingManager.sol";
 import {IStablecoin} from "../src/interfaces/IStablecoin.sol";
 import {Stablecoin} from "../src/Stablecoin.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 
-contract CrossChain is Test {}
+contract CrossChain is Test {
+    CCIPLocalSimulatorFork ccipLocalSimulatorFork;
+    Stablecoin stablecoin;
+    CollateralManager collateralManager;
+    LendingManager lendingManager;
+    ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 100e8);
+    MockV3Aggregator wethPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+    uint8 private constant DECIMALS = 8;
+    int256 private constant ETH_USD_PRICE = 2000e8;
+    uint256 sepoliaFork;
+    uint256 arbSepoliaFork;
+    address alice = makeAddr("alice");
+    address bob = makeAddr("bob");
+    address owner = makeAddr("owner");
+
+    Register.NetworkDetails sepoliaNetworkDetails;
+    Register.NetworkDetails arbSepoliaNetworkDetails;
+
+    function setUp() public {
+        ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        vm.makePersistent(address(ccipLocalSimulatorFork));
+        sepoliaFork = vm.createSelectFork(vm.envString("SEPOLIA_RPC_URL"));
+        arbSepoliaFork = vm.createFork(vm.envString("ARB_SEPOLIA_RPC_URL"));
+
+        sepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+        vm.makePersistent(sepoliaNetworkDetails.rmnProxyAddress);
+        vm.makePersistent(sepoliaNetworkDetails.routerAddress);
+
+        vm.prank(owner);
+        collateralManager = new CollateralManager(
+            address(wethMock),
+            address(wethPriceFeed),
+            sepoliaNetworkDetails.routerAddress,
+            sepoliaNetworkDetails.linkAddress
+        );
+
+        vm.selectFork(arbSepoliaFork);
+        arbSepoliaNetworkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid);
+        vm.makePersistent(arbSepoliaNetworkDetails.rmnProxyAddress);
+        vm.makePersistent(arbSepoliaNetworkDetails.routerAddress);
+
+        vm.startPrank(owner);
+        stablecoin = new Stablecoin();
+        lendingManager = new LendingManager(
+            IStablecoin(address(stablecoin)),
+            arbSepoliaNetworkDetails.routerAddress,
+            arbSepoliaNetworkDetails.linkAddress
+        );
+        vm.stopPrank();
+    }
+}
